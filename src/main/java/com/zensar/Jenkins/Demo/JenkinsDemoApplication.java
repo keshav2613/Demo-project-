055eb0a68ba714b2fa0d6ad13af1bbea387a9c63
Keshav Singh <keshav.singh@zensar.com>
2022-07-06 11:01:25 +0530

    Commit

---
+++ src/main/java/com/zensar/Jenkins/Demo/JenkinsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.zensar.Jenkins.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zensar/Jenkins/Demo/controller/WelcomePage.java
@@ -0,0 +1,15 @@
+package com.zensar.Jenkins.Demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomePage {
+	
+	@GetMapping(value= "/welcome")
+	public String welcome() {
+		
+		return "Welcome to Jenkins";
+	}
+
+}
